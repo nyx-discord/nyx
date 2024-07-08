70e0d19dd2ebe291387a3aaca3829ad20212dce4
Amgelo563 <email@amgelo563.me>
2024-07-08 00:02:20 -0500

    test: add coverage and testEnvironment options to jest configs

--- packages/framework/jest.config.ts
+++ packages/framework/jest.config.ts
@@ -1,6 +1,9 @@
-/* eslint-disable */
+ 
 export default {
   displayName: '@nyx-discord/framework',
   preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/packages/framework',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*Error.ts', '!src/**/*Meta.ts'],
+  testEnvironment: 'node',
 };
