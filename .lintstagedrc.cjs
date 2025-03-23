64d71ada81a443f107dd3dc1ef90406fed033695
Amgelo563 <email@amgelo563.me>
2025-03-23 00:26:09 -0500

    fix: add extension to lintstaged eslint script

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   'packages/**/src/**/*.ts': [
     'prettier --write',
-    'eslint --config ./configs/eslint-config/base --cache --fix',
+    'eslint --config ./configs/eslint-config/base.js --cache --fix',
     'tsc-files --project ./configs/typescript-config/base.json --noEmit',
   ],
 };
