68d78ba21691b7f60b946c1130da18ced24eb742
Amgelo563 <email@amgelo563.me>
2025-08-04 14:06:04 -0500

    feat: improve disableAllComponents readability and also check nested section components

--- packages/framework/src/discord/disableAllComponents.ts
+++ packages/framework/src/discord/disableAllComponents.ts
@@ -1,9 +1,8 @@
 import {
   ActionRowBuilder,
-  ButtonBuilder,
+  ComponentBuilder,
   ComponentType,
   ContainerBuilder,
-  MappedComponentTypes,
   normalizeArray,
   RestOrArray,
   SectionBuilder,
@@ -11,21 +10,27 @@ import {
 
 /** Utility function to disable all components in the provided array. */
 export function disableAllComponents(
-  ...components: RestOrArray<MappedComponentTypes[keyof MappedComponentTypes]>
+  ...components: RestOrArray<ComponentBuilder>
 ): void {
   const array = normalizeArray(components);
   for (const component of array) {
-    if ('setDisabled' in component) {
+    if (
+      'setDisabled' in component
+      && typeof component.setDisabled === 'function'
+    ) {
       component.setDisabled(true);
-      continue;
-    }
-    if (component instanceof SectionBuilder) {
-      if (!(component.accessory instanceof ButtonBuilder)) continue;
-      component.accessory.setDisabled(true);
       continue;
     }
 
     switch (component.data.type) {
+      case ComponentType.Section: {
+        const sectionBuilder = component as SectionBuilder;
+        disableAllComponents(sectionBuilder.components);
+        if (sectionBuilder.accessory) {
+          disableAllComponents(sectionBuilder.accessory);
+        }
+        break;
+      }
       case ComponentType.Container:
         disableAllComponents((component as ContainerBuilder).components ?? []);
         break;
