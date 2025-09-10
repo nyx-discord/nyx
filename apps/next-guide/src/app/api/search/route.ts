4f3dde16d416322e6adfcf55b76f61a2aaad0832
Amgelo563 <email@amgelo563.me>
2025-09-10 00:27:40 -0500

    fix(next-guide): fix search component

--- apps/next-guide/src/app/api/search/route.ts
+++ apps/next-guide/src/app/api/search/route.ts
@@ -1,4 +1,6 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
-export const { GET } = createFromSource(source);
+export const revalidate = false;
+
+export const { staticGET: GET } = createFromSource(source);
